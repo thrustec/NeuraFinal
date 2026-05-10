41a5f657d823d359c546b7c321f9dfeaf3fc46c0
Emre Cantürk <forstorynabon@gmail.com>
2026-05-11 01:43:30 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -25,33 +25,25 @@ class NeuraApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => EvaluationProvider()),
       ],
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, _) {
-          return MaterialApp(
-            title: 'Neura',
-            debugShowCheckedModeBanner: false,
-            theme: NeuraTheme.lightTheme,
-            darkTheme: NeuraTheme.darkTheme,
-            themeMode: themeProvider.themeMode,
-            initialRoute: '/',
-            routes: {
-              '/': (context) => const SplashScreen(),
-              '/login': (context) => const LoginScreen(),
-              '/register': (context) => const RegisterScreen(),
-              '/forgot-password': (context) => const ForgotPasswordScreen(),
-              '/register-clinician': (context) =>
-                  const RegisterClinicianScreen(),
-              '/clinician-agenda': (context) => const ClinicianAgenda(),
-              '/patient-home': (context) =>
-                  const MainScreen(isClinician: false),
-              '/clinician-home': (context) =>
-                  const MainScreen(isClinician: true),
-            },
-          );
+      child: MaterialApp(
+        title: 'Neura',
+        debugShowCheckedModeBanner: false,
+        theme: NeuraTheme.lightTheme,
+        darkTheme: NeuraTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/register': (context) => const RegisterScreen(),
+          '/forgot-password': (context) => const ForgotPasswordScreen(),
+          '/register-clinician': (context) => const RegisterClinicianScreen(),
+          '/clinician-agenda': (context) => const ClinicianAgenda(),
+          '/patient-home': (context) => const MainScreen(isClinician: false),
+          '/clinician-home': (context) => const MainScreen(isClinician: true),
         },
       ),
     );
