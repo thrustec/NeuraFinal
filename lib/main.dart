81b097d0f12065c8a25450b59e2c81c0ae710c3d
Emre Cantürk <forstorynabon@gmail.com>
2026-05-11 01:40:34 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'core/theme.dart';
 import 'providers/auth_provider.dart';
 import 'providers/evaluation_provider.dart';
-import 'providers/theme_provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
